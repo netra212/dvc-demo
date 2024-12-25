b1914368c61d06429b69d7d00a7415fcbba205bd
netr212 <netra200021kcbdr@gmail.com>
2024-12-25 00:13:06 +0000

    add second experiment snapshot taken

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -8,7 +8,10 @@ df = pd.read_csv("https://raw.githubusercontent.com/araj2/customer-database/refs
 df = df.iloc[:, 3:]
 
 # filtering the Memberships columns. 
-df = df[df['Length of Membership'] > 3]
+df = df[df['Length of Membership'] > 1]
+
+# Dropping the Avg. Session Length permanently. 
+df.drop(columns=['Avg. Session Length'], inplace=True)
 
 # Adding the customer.csv data file into the data folder. 
 df.to_csv(os.path.join('data', 'customer.csv'))
